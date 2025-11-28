2a8f2e25f6f1bc4b5e60ee9c8beff3796291463c
sahalshihabudheen-hash <sahalshihabudheen@gmail.com>
2025-11-28 23:07:05 +0530

    Create mockData.js

---
+++ src/lib/mockData.js
@@ -0,0 +1,27 @@
+export const mockSongs = [
+  { id: 1, title: "Cyberpunk 2077 Theme", artist: "Hyper", duration: "3:45", thumbnail: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=800&q=80" },
+  { id: 2, title: "Midnight City", artist: "M83", duration: "4:03", thumbnail: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=800&q=80" },
+  { id: 3, title: "Harder, Better, Faster, Stronger", artist: "Daft Punk", duration: "3:44", thumbnail: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=800&q=80" },
+  { id: 4, title: "Blinding Lights", artist: "The Weeknd", duration: "3:20", thumbnail: "https://images.unsplash.com/photo-1493225255756-d9584f8606e9?w=800&q=80" },
+  { id: 5, title: "Nightcall", artist: "Kavinsky", duration: "4:18", thumbnail: "https://images.unsplash.com/photo-1514525253440-b393452e8d26?w=800&q=80" },
+];
+
+export const mockPlaylists = [
+  { id: 1, name: "Gaming Mix", songCount: 42, duration: "2h 15m" },
+  { id: 2, name: "Chill Vibes", songCount: 18, duration: "1h 05m" },
+  { id: 3, name: "Gym Hype", songCount: 56, duration: "3h 20m" },
+];
+
+export const mockLogs = [
+  { id: 1, action: "Joined voice channel", user: "Bot", time: "2 mins ago", type: "info" },
+  { id: 2, action: "Started playing 'Cyberpunk 2077 Theme'", user: "Bot", time: "2 mins ago", type: "success" },
+  { id: 3, action: "Skipped song", user: "AdminUser", time: "5 mins ago", type: "warning" },
+  { id: 4, action: "Added 'Midnight City' to queue", user: "GuestUser", time: "10 mins ago", type: "info" },
+  { id: 5, action: "Volume set to 80%", user: "AdminUser", time: "15 mins ago", type: "info" },
+];
+
+export const mockServers = [
+  { id: 1, name: "Cyber Lounge", icon: "https://api.dicebear.com/7.x/identicon/svg?seed=Cyber" },
+  { id: 2, name: "Gaming Squad", icon: "https://api.dicebear.com/7.x/identicon/svg?seed=Gaming" },
+  { id: 3, name: "Dev Team", icon: "https://api.dicebear.com/7.x/identicon/svg?seed=Dev" },
+];
